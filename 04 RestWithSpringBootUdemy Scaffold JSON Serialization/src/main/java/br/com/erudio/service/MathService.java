56e9259ec3677bf41e93e6466585a4ee3e66a59f
Raphael Rocha <raphaelrocha.gft@sulamerica.com.br>
2021-03-17 16:41:10 -0300

    implementando HATEOAS - último nível de maturidade

---
+++ 04 RestWithSpringBootUdemy Scaffold JSON Serialization/src/main/java/br/com/erudio/service/MathService.java
@@ -0,0 +1,87 @@
+package br.com.erudio.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exception.ResourceNotFoundException;
+
+@Service
+public class MathService {
+
+	
+	public Double somar(String numberOne, String numberTwo) {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new ResourceNotFoundException("Por favor digite um número!");
+		}
+		
+		double sum = isConvertToDouble(numberOne) + isConvertToDouble(numberTwo);
+		return sum;
+	}
+	
+	public Double subtrair(String numberOne, String numberTwo) {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new ResourceNotFoundException("Por favor digite um número!");
+		}
+		
+		double sub = isConvertToDouble(numberOne) - isConvertToDouble(numberTwo);
+		
+		return sub;
+		
+	}
+	
+	public Double multiplicar(String numberOne, String numberTwo) {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new ResourceNotFoundException("Por favor digite um número!");
+		}
+		
+		double mult = isConvertToDouble(numberOne) * isConvertToDouble(numberTwo);
+		
+		return mult;
+	}
+	
+	public Double dividir(String numberOne, String numberTwo) {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new ResourceNotFoundException("Por favor digite um número!");
+		}
+		double num1 = isConvertToDouble(numberOne);
+		double num2 = isConvertToDouble(numberTwo);
+		if(num1 == 0 || num2 == 0) {
+			throw new ResourceNotFoundException("Número não pode ser 0.");
+		}
+		double div = num1 / num2;
+		
+		return div;
+	}
+	
+	public Double media(String numberOne, String numberTwo) {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new ResourceNotFoundException("Por favor digite um número!");
+		}
+		
+		double media = (isConvertToDouble(numberOne) + isConvertToDouble(numberTwo))/2;
+		
+		return media;
+	}
+	
+	public Double raizQuadrada(String number) {
+		if(!isNumeric(number)) {
+			throw new ResourceNotFoundException("Por favor digite um número!");
+		}
+		
+		double raiz = Math.sqrt(isConvertToDouble(number));
+		
+		return raiz;
+	}
+	
+	
+	private double isConvertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		if(isNumeric(strNumber)) return Double.parseDouble(strNumber);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		return strNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
---
+++ 04 RestWithSpringBootUdemy Scaffold JSON Serialization/target/classes/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (id, address, first_name, gender, last_name) values
+(1, 'Rua Presidente Campo Salles','Raphael', 'Male', 'Rocha'),
+(2, 'Rua Presidente Campo Salles', 'Gabrielle', 'Female', 'Rosa'),
+(3, 'Rua Presidente Campo Salles', 'Maria', 'Female', 'Rosa'),
+(6, 'Rua Borges Vieira, 56', 'Joelma', 'Female', 'Rodrigues Nadur'),
+(9, 'Rua Cruz Maltina, 56', 'Ricardo', 'Male', 'Coelho');
